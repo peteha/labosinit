2a5d3f23ec8b0f3950c80c4bbd3024967969bdcd
Peter Hauck <peteha@douglas.local>
2021-05-23 04:39:04 +0000

    Updated with Command Options

--- labinit.sh
+++ labinit.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
 ## Created by Peter Hauck for lab build.
-
-sudo su
-setuser=$1
+while getopts u:p:d:c:r o
+do	case "$o" in
+	u)	setuser="$OPTARG";;
+	p)  setpasswd="$OPTARG";;
+	d)	dockerinst="yes";;
+	c)  certbotinst=$OPTARG;;
+	r)  rebooinst="yes"
+	[?])	print >&2 "Usage: $0 [-u user] [-p passwd] [-d] [-c cloudflarednsapikey] [-r] ..."
+		exit 1;;
+	esac
+done
 TIMEZONE="Australia/Brisbane"
 
 # Add User
-adduser $setuser
-usermod -aG sudo $setuser
-
-# Set no sudo passwd
-echo "$setuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo -k
+if [ setuser ]
+then 
+	useradd $setuser --create-home --shell /bin/bash --groups sudo
+	echo "$setuser:$setpasswd" | sudo chpasswd
+	# Set no sudo passwd
+	echo "$setuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+	sudo -k
+fi
 
 # Set Timezone
 echo $TIMEZONE > /etc/timezone
@@ -26,13 +36,18 @@ apt upgrade -y
 # Install Base Packages
 apt install certbot python3-certbot-dns-cloudflare nfs-common
 
-if [ $2 = 'docker' ]
+if [ dockerinst ]
 then
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo \
 	  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	apt-get update
-	apt-get install docker-ce docker-ce-cli containerd.io
-	sudo groupadd docker
+	apt-get install docker-ce docker-ce-cli containerd.io -y
+	groupadd docker
 	usermod -aG docker $setuser
+fi
+if [ rebootinst ]
+then
+	reboot
+fi

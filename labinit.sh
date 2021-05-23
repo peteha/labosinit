b466338e778083005951a15c659e2fd696b954f7
Peter Hauck <peteha@douglas.local>
2021-05-23 05:05:22 +0000

    Update for checking

--- labinit.sh
+++ labinit.sh
@@ -1,53 +1,52 @@
 #!/bin/sh
 
 ## Created by Peter Hauck for lab build.
-while getopts u:p:d:c:r o
+while getopts u:p:d:c:r:t o
 do	case "$o" in
 	u)	setuser="$OPTARG";;
 	p)  setpasswd="$OPTARG";;
 	d)	dockerinst="yes";;
 	c)  certbotinst=$OPTARG;;
+	t)  TIMEZONE="Australia/Brisbane";;
 	r)  rebooinst="yes";;
 	[?])	print >&2 "Usage: $0 [-u user] [-p passwd] [-d] [-c cloudflarednsapikey] [-r] ..."
 		exit 1;;
 	esac
 done
-TIMEZONE="Australia/Brisbane"
+
 
 # Add User
-if [ setuser ]
+if [ -z $setuser ]
 then 
 	useradd $setuser --create-home --shell /bin/bash --groups sudo
-	echo "$setuser:$setpasswd" | sudo chpasswd
+	echo "$setuser:$setpasswd" | chpasswd
 	# Set no sudo passwd
 	echo "$setuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-	sudo -k
 fi
 
-# Set Timezone
-echo $TIMEZONE > /etc/timezone
-cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
-
+if [ -z $TIMEZONE ]
+then
+	# Set Timezone
+	echo "## Setting Timezone $TIMEZONE ##"
+	echo $TIMEZONE > /etc/timezone
+	cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+fi
 
 # Update OS
-apt update
-apt upgrade -y
+#apt update
+#apt upgrade -y
 
 # Install Base Packages
-apt install certbot python3-certbot-dns-cloudflare nfs-common
+#apt install certbot python3-certbot-dns-cloudflare nfs-common
 
-if [ dockerinst ]
+if [ -z $dockerinst ]
 then
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	echo \
-	  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt-get update
-	apt-get install docker-ce docker-ce-cli containerd.io -y
+	curl -fsSL https://get.docker.com -o get-docker
+	sh get-docker.sh
 	groupadd docker
 	usermod -aG docker $setuser
 fi
-if [ rebootinst ]
+if [ -z $rebootinst ]
 then
 	reboot
 fi

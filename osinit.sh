07c7ae4777445ae0cb68180722703f4484a3df73
peteha <peter@pggb.net>
2022-10-01 10:15:04 +1000

    Updates for pretty

--- osinit.sh
+++ osinit.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-
+echo "## Setting Up OS Build Directory ##"
 # Create Base DIR
 mkdir -p /opt/osbuild
 cd /opt/osbuild
 ## 
+echo "## Installing Scripts ##"
 curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/osinit.sh --output osinit.sh
 chmod +x osinit.sh
+curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/certbuild.sh --output certbuild.sh
+chmod +x certbuild.sh
 
 if [ ! -f hostbuild.env ]; then
-    echo ## No hostbuild.env file available ##
+    echo "## No hostbuild.env file available ##"
     curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output hostbuild.env
     sudo apt install nano
     nano hostbuild.env
 fi
-
+echo "## Using hostbuild.env ##"
 source hostbuild.env
 source /etc/lsb-release
 
 cur_tz=`cat /etc/timezone`
 fullhn="$buildhostname.$domain"
+
+echo "## Building For $fullhn ##"
 
 echo "## Setting Up Environment ##"
 echo

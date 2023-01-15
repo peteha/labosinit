84a0899afd894d8a5f5b0c265fdc3ad2308f9575
Peter Hauck <peter@pggb.net>
2023-01-15 13:54:31 +1000

    22

--- osinit.sh
+++ osinit.sh
@@ -10,6 +10,7 @@ chmod +x osinit.sh
 curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/certbuild.sh --output certbuild.sh
 chmod +x certbuild.sh
 
+## 
 if [ ! -f hostbuild.env ]; then
     echo "## No hostbuild.env file available ##"
     curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output hostbuild.env

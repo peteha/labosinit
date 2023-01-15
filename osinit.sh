57be286ae321a3b9ffb2f94c729d3150d8bcd0d5
Peter Hauck <peter@pggb.net>
2023-01-15 14:13:44 +1000

    Update

--- osinit.sh
+++ osinit.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo "## Setting Up OS Build Directory ##"
 # Create Base DIR
-exit
-
 mkdir -p /opt/osbuild
 cd /opt/osbuild
 ##
 echo "## Installing Scripts ##"
-exit
 curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/osinit.sh --output osinit.sh
 chmod +x osinit.sh
 curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/certbuild.sh --output certbuild.sh

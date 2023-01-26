19ed10c6136beec9efd289d2767c99cf746cc9e2
Peter Hauck <peter@pggb.net>
2023-01-26 21:08:06 +1000

    d

--- addcftoken.sh
+++ addcftoken.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source hostbuild.env
 if [ ! -f /home/$username/cfcred/cf-api-token.ini ]
 then
     echo -n "Enter CloudFlare API Token: "
@@ -6,6 +7,7 @@ then
     mkdir -p /home/$username/cfcred
 	echo dns_cloudflare_api_token = "$cfapitoken" > /home/$username/cfcred/cf-api-token.ini
 	chmod 600 /home/$username/cfcred/cf-api-token.ini
+    echo "## Cloudflare Token Added to /home/$username/cfcred/cf-api-token.ini ##"
 fi
 if ! command -v certbot &> /dev/null; then
     echo "## No certbot installed ##"

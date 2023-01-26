06d0a5fb13b62cac47349a833125852d2b44abd2
Peter Hauck <peter@pggb.net>
2023-01-26 21:10:24 +1000

    dzz

--- getcert.py
+++ getcert.py
@@ -1,19 +1,10 @@
 import certbot.main
 import os
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 emailadd = "admin@pggb.net"
 fullhostname = "pi3.pggb.net"
 cfcredir = "/root/cfcred/cf-api-token.ini"
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/main
 if os.path.exists(_cfcredir, _addr, _fhn):
     certbot.main.main([
         "--non-interactive",
@@ -25,8 +16,3 @@ if os.path.exists(_cfcredir, _addr, _fhn):
         "certonly",
         "--preferred-challenges", "dns-01"
     ])
-else:
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main

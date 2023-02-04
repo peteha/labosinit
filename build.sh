528f82f26af461f24bb032b72599b8dc365e1c63
Peter Hauck <peter@pggb.net>
2023-02-04 03:35:19 +0000

    sss

---
+++ build.sh
@@ -0,0 +1,3 @@
+inst_pkgs="python3 python3-certbot python3-certbot-dns-cloudflare nfs-common python3-pip"
+bootfile="/boot/cmdline.txt"
+k8_params=" group_enable=cpuset cgroup_enable=memory cgroup_memory=1"
--- nbuild.sh
+++ nbuild.sh
@@ -2,13 +2,13 @@
 echo "## Setting Up OS Build Directory ##"
 # Create Base DIR
 mkdir -p /opt/osbuild
-cd /opt/osbuild
+cd /opt/osbuild || exit
 
 ## Install host build file
 
-if [ ! -f hostbuild.env ]; then
+if [ ! -f build.sh ]; then
     echo "## No hostbuild.env file available ##"
-    curl -fs https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output hostbuild.env
+    curl -fs "https://raw.githubusercontent.com/peteha/labosinit/main/hostbuild.env --output build.sh"
     ## nano hostbuild.env
 fi
 
@@ -18,21 +18,67 @@ source hostbuild.env
 
 ## Establish user credentials
 
-read -p "Username: " user
-read -sp "Password: " password
+read -rp "Username: " user
+read -rsp "Password: " password
 
 if id "$user" >/dev/null 2>&1; then
-  usrchgdef = "Y"
-  read -p "Change existing user password: (default: $usrchgdef): " usrchg
+  usrchgdef="Y"
+  echo
+  read -rp "Change existing user password: (default: $usrchgdef): " usrchg
   usrchg=${usrchg:-$usrchgdef}
   usrchg=$(echo "$usrchg" | tr '[:lower:]' '[:upper:]')
-  if usrchg = "Y"; then
+  if [ "$usrchg" == "Y" ]; then
     echo "User $user exists. Changing password..."
     echo "$user:$password" | chpasswd
+  else
+    echo "## Password not change ##"
   fi
 else
+  echo
   echo "User $user does not exist. Adding user and password..."
-  useradd "$user"
+  useradd "$user" --create-home --shell /bin/bash --groups sudo
   echo "$user:$password" | chpasswd
-  usermod -aG sudo "$user"
+  if grep -Fxq "$user ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
+    then
+      echo "## Already SUDO ##"
+    else
+      echo "Set SUDO Happening for $user"
+      echo "$user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    fi
 fi
+echo "## Getting SSH Keys ##"
+curl -s "https://github.com/$user.keys" > "/tmp/$user.keys"
+gitpk_dl=$(cat "/tmp/$user.keys")
+if [ $? -eq 0 ] ; then
+  if grep -Fxq "$gitpk_dl" "/home/$user/.ssh/authorized_keys"; then
+    echo "## Already in authorized_keys ##"
+  else
+    mkdir -p "/home/$user/.ssh"
+    cat "/tmp/$user.keys" >> "/home/$user/.ssh/authorized_keys"
+    echo "The SSH key was added successfully."
+  fi
+else
+  echo "Failed to retrieve the SSH key. Please check your GitHub username."
+fi
+echo "## Setting K8 parameters ##"
+# shellcheck disable=SC2154
+if [ -f "$bootfile" ]; then
+  if grep -q "$k8_params" "$bootfile"; then
+    echo
+    echo "## Params for K8 already in $bootfile ##"
+  else
+    sed -i '$ s/$/ '"$k8_params"'/' "$bootfile"
+    echo "## Params for K8 added to $bootfile ##"
+  fi
+else
+  echo "## bootfile not found ##"
+fi
+echo
+echo "Updating apt"
+apt update
+if [ -z "${!inst_pkgs}" ]; then
+  echo "## Package list does not exist ##"
+else
+  apt install $inst_pkgs -y
+fi
+

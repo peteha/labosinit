dcd143540bbd691fb0cf66b26ada2a5876e6818c
Peter Hauck <peter@pggb.net>
2023-02-05 21:13:41 +1000

    Changes to need-restart

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-inst_pkgs="certbot python3 python3-certbot-dns-cloudflare nfs-common python3-pip nano"
+inst_pkgs="certbot python3 python3-certbot-dns-cloudflare nfs-common python3-pip nano nfs ansible open-iscsi util-linux"
 bootfile="/boot/firmware/cmdline.txt"
 k8_params=" group_enable=cpuset cgroup_enable=memory cgroup_memory=1"
 baserelease="22"

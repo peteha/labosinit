65b502b615baa4223851c099ea246e4836c72188
Peter Hauck <peter@pggb.net>
2023-02-05 21:09:24 +1000

    Changes to need-restart

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 inst_pkgs="certbot python3 python3-certbot-dns-cloudflare nfs-common python3-pip nano"
-bootfile="/boot/cmdline.txt"
+bootfile="/boot/firmware/cmdline.txt"
 k8_params=" group_enable=cpuset cgroup_enable=memory cgroup_memory=1"
 baserelease="22"
